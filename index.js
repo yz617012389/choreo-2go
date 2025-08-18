43ab6af53f9fc08b5a52c51837f876dfd1965efa
yz617012389 <y617012389@outlook.com>
2025-08-19 02:45:31 +0800

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const UPLOAD_URL = process.env.UPLOAD_URL || '';      // 节点或订阅自动上传地址,需填写部署Merge-sub项目后的首页地址,例如：https://merge.ct8.pl
-const PROJECT_URL = process.env.PROJECT_URL || '';    // 需要上传订阅或保活时需填写项目分配的url,例如：https://google.com   
-const AUTO_ACCESS = process.env.AUTO_ACCESS || false; // false关闭自动保活，true开启,需同时填写PROJECT_URL变量
+const PROJECT_URL = process.env.PROJECT_URL || 'https://6e73fef3-f485-407a-8fea-51bfb4362f98.e1-us-east-azure.choreoapps.dev';    // 需要上传订阅或保活时需填写项目分配的url,例如：https://google.com   
+const AUTO_ACCESS = process.env.AUTO_ACCESS || true; // false关闭自动保活，true开启,需同时填写PROJECT_URL变量
 const FILE_PATH = process.env.FILE_PATH || './tmp';   // 运行目录,sub节点文件保存目录
 const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅路径
 const PORT = process.env.SERVER_PORT || process.env.PORT || 8080;  // http服务端口，不用改       
